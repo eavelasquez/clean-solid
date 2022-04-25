7fce18ec670ad771718770cd56e6d0c193fed211
Esteban Velasquez <eavc.30@gmail.com>
2022-04-25 07:43:36 -0500

    feat(back): add new property to product class
    - end console group.
    - add formatPrice function.
    - validate boolean data type.
    - import 05-dry file into main file.

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -4,15 +4,17 @@ class Product {
   constructor(
     public name: string = '',
     public price: number = 0,
+    public inStock: boolean = false,
     public size: Size = '',
-  ) {}
+  ) { }
 
   isProductReady(): boolean {
-    // No Dry
+    // No DRY (Don't Repeat Yourself)
     // if (this.name.length <= 0) throw Error('name is empty');
     // if (this.price <= 0) throw Error('price is zero or negative');
     // if (this.size.length <= 0) throw Error('size is empty');
 
+    // DRY (Don't Repeat Yourself)
     for (const key in this) {
       switch (typeof this[key]) {
         case 'string':
@@ -27,19 +29,29 @@ class Product {
             throw Error(`${key} is zero or negative`);
           }
           break;
+        case 'boolean':
+          // validate boolean is true
+          if (!(<boolean><unknown>this[key])) {
+            throw Error(`${key} is false`);
+          }
+          break;
 
         default:
-          throw Error(`${typeof this[key]} is not a string or number`);
+          throw Error(`${typeof this[key]} is not a string, number or boolean`);
       }
     }
 
     return true;
   }
 
+  formatPrice(): string {
+    return `$ ${this.price.toFixed(2)}`;
+  }
+
   toString(): string {
     if (!this.isProductReady()) return 'Product is not ready';
 
-    return `${this.name} (${this.price}), ${this.size}`;
+    return `${this.name} (${this.formatPrice()}), ${this.size} size, in stock: ${this.inStock}`;
   }
 }
 
@@ -47,12 +59,13 @@ class Product {
  * self-executing anonymous function
  */
 ((): void => {
-  const bluePants = new Product('blue pants', 100, 'large');
-  const redShirt = new Product('red shirt', 50, 'medium');
-  const greenShoes = new Product('green shoes', 150, 'small');
+  const bluePants = new Product('blue pants', 100, true, 'large');
+  const redShirt = new Product('red shirt', 50, true, 'medium');
+  const greenShoes = new Product('green shoes', 150, true, 'small');
 
   const products: Product[] = [bluePants, redShirt, greenShoes];
 
   console.group('products');
   products.forEach(product => console.log(product.toString()));
+  console.groupEnd();
 })();
