4b1ca14205378de4b0daa34ccb5ef17369e1f679
Esteban Velasquez <eavc.30@gmail.com>
2022-04-26 07:05:06 -0500

    feat(back): add 06-classes-c file
    - with applying the single responsibility principle.
    - prioritize composition over inheritance.

---
+++ src/clean-code/06-classes-c.ts
@@ -0,0 +1,110 @@
+(() => {
+  // With applying the Single Responsibility Principle
+  // Prioritize composition over inheritance
+
+  type Gender = 'male' | 'female';
+
+  interface PersonProps {
+    birthDate: Date;
+    gender: Gender;
+    name: string;
+  }
+
+  class Person {
+    public birthDate: Date;
+    public gender: Gender;
+    public name: string;
+
+    constructor({ birthDate, gender, name }: PersonProps) {
+      this.birthDate = birthDate;
+      this.gender = gender;
+      this.name = name;
+    }
+
+    greet(): string {
+      return `Hello, ${this.name}`;
+    }
+  }
+
+  interface UserProps {
+    email: string;
+    role: string;
+  }
+
+  class User {
+    public email: string;
+    public lastAccessDate: Date;
+    public role: string;
+
+    constructor({ email, role }: UserProps) {
+      this.email = email;
+      this.lastAccessDate = new Date();
+      this.role = role;
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  interface SettingsProps {
+    lastOpenFolder: string;
+    workingDirectory: string;
+  }
+
+  class Settings {
+    public lastOpenFolder: string;
+    public workingDirectory: string;
+
+    constructor({ lastOpenFolder, workingDirectory }: SettingsProps) {
+      this.lastOpenFolder = lastOpenFolder;
+      this.workingDirectory = workingDirectory;
+    }
+  }
+
+  interface UserSettingsProps {
+    birthDate: Date;
+    email: string;
+    gender: Gender;
+    lastOpenFolder: string;
+    name: string;
+    role: string;
+    workingDirectory: string;
+  }
+
+  // Note: this class is a composition of the other classes
+  class UserSettings {
+    public person: Person;
+    public user: User;
+    public settings: Settings;
+
+    constructor({
+      birthDate,
+      email,
+      gender,
+      lastOpenFolder,
+      name,
+      role,
+      workingDirectory,
+    }: UserSettingsProps) {
+      this.person = new Person({ name, gender, birthDate });
+      this.user = new User({ email, role });
+      this.settings = new Settings({ lastOpenFolder, workingDirectory });
+    }
+  }
+
+  const userSettings = new UserSettings({
+    birthDate: new Date('2001-01-01'),
+    email: 'ai@google.com',
+    gender: 'female',
+    lastOpenFolder: '/home',
+    name: 'Ai',
+    role: 'admin',
+    workingDirectory: '/usr/home',
+  });
+
+  console.log(userSettings.person.greet(), {
+    userSettings,
+    credentials: userSettings.user.checkCredentials(),
+  });
+})();
