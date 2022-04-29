64c02289188fa364c915fb32912481b0cd9bd422
Esteban Velasquez <eavc.30@gmail.com>
2022-04-29 08:43:05 -0500

    feat(back): add 05-dependency files
    - example of not applying the dependency inversion principle.

---
+++ src/solid/05-dependency-a.ts
@@ -0,0 +1,7 @@
+import { PostService } from './05-dependency-b';
+
+(async (): Promise<void> => {
+  const postService = new PostService();
+  const posts = await postService.getPosts();
+  console.log(posts);
+})();
---
+++ src/solid/05-dependency-b.ts
@@ -0,0 +1,23 @@
+import { LocalDatabaseService } from './05-dependency-c';
+
+interface Post {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+}
+
+export class PostService {
+  private posts: Post[] = [];
+
+  constructor() { }
+
+  async getPosts(): Promise<Post[]> {
+    if (this.posts.length === 0) {
+      const posts = await new LocalDatabaseService().getFakePosts();
+      this.posts = posts;
+    }
+
+    return this.posts;
+  }
+}
